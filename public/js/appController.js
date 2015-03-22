dffd1a33fef142b0b2565afd33fcb8db6a6fa4b6
veliancreate <julian_veling@hotmail.com>
2015-03-22 17:54:26 +0000

    prior to protractor

--- public/js/appController.js
+++ public/js/appController.js
@@ -1,7 +1,18 @@
 toDoList = angular.module('toDoList',[]);
 toDoList.controller('ToDoController', ['$scope','$http', function($scope, $http){
+  
+
   $http.get('/todolist').success(function(response){
+    console.log(response);
     $scope.todolist = response;
   });
+
+  $scope.addTodo = function(){
+    $http.post('/todolist', $scope.todo).success(function(response){
+      console.log(response); 
+      $scope.todolist.push(response); 
+      $scope.todo = "";
+    });
+  };
 }]);
 
--- spec/frontSpec.js
+++ spec/frontSpec.js
@@ -1,6 +1,8 @@
 describe('To_do_list', function(){
+  
   it('should have a title', function(){
-    browser.get('http://localhost:3000');
+    browser.get('/');
     expect(browser.getTitle()).toEqual('To Do List');
   });
+
 });
