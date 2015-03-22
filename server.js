8d678d5053e69475941393d269fdc22399f993a0
veliancreate <julian_veling@hotmail.com>
2015-03-22 15:29:08 +0000

    removed mongoose, now using mongojs

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose'); // mongoose for mongodb
 var morgan = require('morgan');  // log requests to the console (express4)
 var bodyParser = require('body-parser');  // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
-mongoose.connect('mongodb://localhost/todolist')     // connect to mongoDB database
+var mongojs = require('mongojs');
+var db = mongojs('todolist', ['todolist']);
 
 app.set('views', __dirname + '/public');
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
@@ -17,16 +16,6 @@ app.use(methodOverride());
 app.set('view engine', 'ejs');
 var server = require('http').createServer(app);
 
-// Mongo schema
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  var todolist = mongoose.Schema({
-    content: String
-  })
-});
-
 // listen (start app with node server.js) ======================================
 
 app.get('/', function(req, res) {
@@ -34,16 +23,10 @@ app.get('/', function(req, res) {
 });
 
 app.get('/todolist', function(req, res){
-  todolist = [{
-      content: "Walk dog"
-    },
-    {
-      content: "Take out rubbish"
-    }];
-  res.json(todolist);  
+  db.todolist.find(function(err, docs){
+    res.json(docs);
+  });
 });
-
-
 
 app.listen(3000);
 console.log("App listening on port 3000");
