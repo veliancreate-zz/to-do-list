e560d4e349d902f5470312c17ca1a25a828bf7f2
veliancreate <julian_veling@hotmail.com>
2015-03-22 18:25:33 +0000

    protractor doing stuff. now need to refactor

--- spec/frontSpec.js
+++ spec/frontSpec.js
@@ -1,8 +1,26 @@
 describe('To_do_list', function(){
   
-  it('should have a title', function(){
-    browser.get('/');
-    expect(browser.getTitle()).toEqual('To Do List');
-  });
+  describe('index', function(){
+
+    beforeEach(function(){
+      browser.get('/#');
+    });
+
+    it('should have a title', function(){
+      expect(browser.getTitle()).toEqual('To Do List');
+    });
+
+    it('should have a button that adds todos', function(){
+      expect(browser.getTitle()).toEqual('To Do List');
+    });
+
+    it('can add todos', function(){
+      element(by.model('todo.content')).sendKeys('Do something!');
+      element(by.css('#add-todo')).click();
+      var todoList = element.all(by.repeater('item in todolist'));
+      expect(todoList.count()).toEqual(1);
+      expect(todoList.get(1).getText()).toEqual('Do something!');
+    });  
+  });  
 
 });
