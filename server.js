680053a6daa9d38a69d9adc570096ec46ad2da9c
Julian Veling <julian_veling@hotmail.com>
2015-06-08 17:30:02 +0100

    heroku

--- server.js
+++ server.js
@@ -64,8 +64,8 @@ app.put('/todolist/:id', function (req, res) {
 });
 
 
-app.listen(3000);
-console.log("App listening on port 3000");
+app.listen(process.env.PORT || 3000);
+console.log("App listening");
 
 
 module.exports = server;
